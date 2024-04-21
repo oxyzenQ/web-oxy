29adf44d181d5e71fdb172321438ae4d12eefae5
oxyrezz <130107241+oxyrezz-code@users.noreply.github.com>
2024-04-22 00:23:28 +0700

    Add files via upload

---
+++ web-nightly-r1/Currency/cc.js
@@ -0,0 +1,63 @@
+const fromCur = document.querySelector(".from-select");
+const toCur = document.querySelector(".to-select");
+const getBtn = document.querySelector("form button");
+const exIcon = document.querySelector("form .reverse");
+const amount = document.querySelector("form input");
+const exRateTxt = document.querySelector("form .result");
+
+// Event listener for currency dropdowns (select)
+
+[fromCur, toCur].forEach((select, i) => {
+    for (let curCode in Country_List) {
+        const selected = (i === 0 && curCode === "USD") || (i === 1 && curCode === "EUR") ? "selected" : "";
+        select.insertAdjacentHTML("beforeend", `<option value="${curCode}" ${selected}>${curCode}</option>`);
+    }
+    select.addEventListener("change", () => {
+        const code = select.value;
+        const imgTag = select.parentElement.querySelector("img");
+        imgTag.src = `https://flagcdn.com/48x36/${Country_List[code].toLowerCase()}.png`;
+    });
+});
+
+//function to get exchange rate from api
+
+async function getExchangeRate() {
+    const amountVal = amount.value || 1;
+    exRateTxt.innerText = "Getting exchange rate...";
+    try {
+        const response = await fetch(`https://v6.exchangerate-api.com/v6/de1695208ebf652f2f84fe41/latest/${fromCur.value}`);
+        const result = await response.json();
+        const exchangerate = result.conversion_rates[toCur.value];
+        const totalExRate = (amountVal * exchangerate).toFixed(2);
+
+        // Format the totalExRate using Intl.NumberFormat
+        const formatter = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: toCur.value,
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+        });
+
+        exRateTxt.innerText = `1.00 ${fromCur.value} = ${formatter.format(totalExRate)}`;
+    } catch (error) {
+        exRateTxt.innerText = "Something went wrong...";
+    }
+}
+
+//event listener for button and exchange icon click
+
+window.addEventListener("load", getExchangeRate);
+getBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    getExchangeRate();
+});
+
+exIcon.addEventListener("click", () => {
+    [fromCur.value, toCur.value] = [toCur.value, fromCur.value];
+    [fromCur, toCur].forEach((select) => {
+        const code = select.value;
+        const imgTag = select.parentElement.querySelector("img");
+        imgTag.src = `https://flagcdn.com/48x36/${Country_List[code].toLowerCase()}.png`;        
+    });
+    getExchangeRate();
+});
